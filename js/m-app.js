f4b627376ce2398e77180e2bb904a1fcfb577e02
Marc Anderson <marc_anderson_1981@yahoo.com>
2015-08-25 15:17:42 -0500

    JS game

---
+++ js/m-app.js
@@ -0,0 +1,339 @@
+// Draw the enemy and player objects on the screen
+Object.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+//Reset player to beginning position
+Object.prototype.reset = function() {
+  player.x = 450;
+  player.y = 500;
+  ctx.clearRect(0, 0, 500, 500);
+}
+
+//If player reaches water, image changes, announces winning.
+Object.prototype.win = function(){
+    player.sprite = "images/winner.png";
+    player.x = 600;
+    player.y = -50;
+    moveRight = false;
+    moveLeft = false;
+    lives = 3;
+    setTimeout(function(){ctx.clearRect(600, -50, 110, 110); ctx.clearRect(200, 0, 300, 200); player.sprite = 'images/elfin.png'; player.reset(); beginNewGame(); }, 5000);
+}
+
+Object.prototype.timesUp = function(){
+    player.sprite = "images/grave.png";
+    player.x = 405;
+    player.y = 325;
+    setTimeout(function(){ alert("You have have failed to complete your mission in the allotted time. I suggest you buy a watch. Game Over.") }, 500);
+    setTimeout(function(){player.sprite = 'images/elfin.png'; player.reset(); beginNewGame();}, 3000);
+}
+//If player is killed the number of lives decrements, and if no more lives, game is over. Otherwise ask player to try again.
+Object.prototype.dead = function(){
+    lives = lives - 1;
+    gemsObtained = 0;
+    if(lives <= 0){
+        player.sprite = "images/grave.png";
+        player.x = 405;
+        player.y = 325;
+        setTimeout(function(){ alert("You have ceased to be. Game Over.") }, 700);
+        setTimeout(function(){player.sprite = 'images/elfin.png'; player.reset(); beginNewGame();}, 5000);
+    } else {
+        player.sprite = "images/dead.png";
+        player.x = 405;
+        player.y = 325;
+        setTimeout(function(){
+            if(confirm("Would you like to play again?")){
+                ctx.clearRect(450, 500, 100, 100);
+                player.sprite = 'images/elfin.png';
+                player.reset();
+            } else {
+                alert("Fine, be that way.");
+                player.x = 405;
+                player.y = 325;
+                player.sprite = 'images/grave.png';
+                setTimeout(function(){player.sprite = 'images/elfin.png'; player.reset(); beginNewGame();}, 5000);
+            }
+        }, 500);
+    }
+}
+
+var begin = false;
+var lives = 3;
+var livesRemaining = document.getElementById('lives');
+var gemsObtained = 0;
+var scoreDisplay = document.getElementById('score');
+
+// Enemies the player must avoid
+var Enemy = function(x,y) {
+
+    // The image/sprite for enemies
+    this.sprite = 'images/enemy-bug.png';
+
+    //x and y coordinates and movement speed
+    this.x = x;
+    this.y = y;
+    this.speed = Math.floor((Math.random() * 100) + 100);
+}
+
+var FlyingEnemy = function(x,y){
+    this.sprite = 'images/flying-bug.png';
+    this.x = x;
+    this.y = y;
+    this.speed = Math.floor((Math.random() * 100) + 100);
+}
+
+var Gatekeeper = function(x,y) {
+    this.sprite = 'images/bee.png';
+    //x and y coordinates and movement speed
+    this.x = x;
+    this.y = y - 10;
+    this.speed = Math.floor((Math.random() * 100) + 100);
+}
+
+Enemy.prototype.update = function(dt) {
+    //if the enemy crosses off screen, reset its position. Otherwise, it keeps running.
+    if(this.x <= 850){
+        this.x += this.speed * dt;
+    }else{
+        this.x = -2;
+    }
+
+    //If the player comes within 30px of an enemy's x and y coordinates, reset the game
+    if(player.x >= this.x - 30 && player.x <= this.x + 30){
+        if(player.y >= this.y - 30 && player.y <= this.y + 30){
+            alert("Oh no! You're totally allergic to bugs! Bummer!");
+            player.dead();
+        }
+    }
+}
+
+FlyingEnemy.prototype.update = function(dt) {
+    //if the enemy crosses off screen, reset its position. Otherwise, it keeps running.
+    if(this.x >= -50){
+        this.x -= this.speed * dt;
+    }else{
+        this.x = 910;
+    }
+
+    //If the player comes within 30px of an enemy's x and y coordinates, reset the game
+    if(player.x >= this.x - 30 && player.x <= this.x + 30){
+        if(player.y >= this.y - 30 && player.y <= this.y + 30){
+            alert("Flying bugs are the worst.")
+            player.dead();
+        }
+    }
+}
+
+var Weeds = function(x,y) {
+    this.sprite = 'images/weeds.png';
+    //x and y coordinates and movement speed
+    this.x = x;
+    this.y = y;
+    this.speed = 100;
+}
+
+
+//Controls up and down movement of weeds
+var moveUp = true;
+var moveDown = false;
+Weeds.prototype.update = function(dt){
+    if(moveUp && this.y >= 550){
+        this.y -= this.speed * dt;
+    } else {
+        moveUp = false;
+        moveDown = true;
+    }
+    if(moveDown && this.y <= 650){
+        this.y += this.speed * dt;
+    } else {
+        moveDown = false;
+        moveUp = true;
+    }
+    ctx.clearRect(50, 600, 220, 220);
+//If player is caught in weeds, player dies.
+    if(player.x >= this.x - 30 && player.x <= this.x + 30){
+        if(player.y >= this.y - 50 && player.y <= this.y + 40){
+            alert("Oh no! You have been ensnared by the cursed Weeds of Doom!");
+            player.dead();
+        }
+    }
+}
+
+//Sets default movement pattern for Gatekeeper
+var moveRight = true;
+var moveLeft = false;
+//controls the movement of Bee Gatekeeper
+Gatekeeper.prototype.update = function(dt) {
+    //Sets bounds and direction of movement
+    if(moveLeft || moveRight){
+        this.sprite = "images/bee.png";
+    } else {
+        this.sprite = "images/goodbye.png";
+        this.x = 220;
+        this.y = 30;
+    }
+    if(moveRight && this.x <= 600){
+            this.x += this.speed * dt;
+    } else {
+        moveRight = false;
+        moveLeft = true;
+    }
+    if(moveLeft && this.x >= 200){
+        this.x -= this.speed *dt;
+    } else {
+        moveLeft = false;
+        moveRight = true;
+    }
+
+
+    //If the player comes in contact with GateKeeper's x and y coordinates, prompt question
+    if(player.x >= this.x - 30 && player.x <= this.x + 30){
+        if(player.y >= this.y - 30 && player.y <= this.y + 30){
+            var answer = "1";
+
+            var reply = prompt("As I was going to St. Ives, I met a man with seven wives. The seven wives had seven cats, each of which had seven rats. The rats all carried seven hats, stacked upon their heads they sat. Now tell me, how many were going to St.Ives?");
+
+            if(reply == answer){
+                alert("Curses! Foiled again! Proceed.")
+                if(gemsObtained >= 3){
+                    player.win();
+                    this.sprite = "images/goodbye.png";
+                    this.x = 220;
+                    this.y = 30;
+                } else {
+                    alert("You need at least three gems to pay for your voyage!");
+                }
+            } else {
+                alert("Ha-ha-ha! You fool! I said 'As I was going to St. Ives.' There was only 1 person going to St. Ives! Now taste my venomous stinger!");
+                player.dead();
+            }
+        }
+    }
+}
+
+//Gem
+var Gemstone = function(x,y){
+    this.sprite = 'images/gem.png';
+    this.x = x;
+    this.y = y;
+
+}
+
+//If the player comes within 40px of an gems's x and y coordinates, gem dissappears and score increments by one.
+Gemstone.prototype.update = function(){
+   if(player.x >= this.x - 40 && player.x <= this.x + 40){
+        if(player.y >= this.y - 40 && player.y <= this.y + 40){
+            this.x = Math.floor((Math.random() * 700));
+            this.y = Math.floor((Math.random() * 505));
+            ctx.clearRect(32, 0, 520, 220);
+            ++gemsObtained;
+        }
+    }
+}
+
+var gemstone = new Gemstone(606, 404);
+
+//Controls random positioning of gemstone that changes after 7 seconds.
+var randomX = Math.floor((Math.random() * 700));
+var randomY = Math.floor((Math.random() * 505));
+var randomMove = function(){
+        if(randomY >= 100 && randomX >= (20 + player.x) ){
+            gemstone.x = randomX;
+            gemstone.y = randomY;
+            } else {
+                gemstone.x = 750;
+                gemstone.y = 110;
+            }
+        }
+
+setInterval(function(){ randomMove(); }, 7000);
+
+
+// Player class and initial x and y coordinates
+var Player = function(){
+    this.sprite = 'images/elfin.png';
+    this.x = 450;
+    this.y = 500;
+}
+
+//Update player position
+Player.prototype.update = function(){
+    if(begin){//if left key is pressed and player is not on edge of map, pressed decrement x
+        if(this.ctlKey === 'left' && this.x > 0){
+            this.x = this.x - 50;
+        //if right key is pressed and player is not on edge of map increment x
+        }else if(this.ctlKey === 'right' && this.x != 800){
+            this.x = this.x + 50;
+        //if up key is pressed increment y
+        }else if(this.ctlKey === 'up'){
+            this.y = this.y - 50;
+        //if down key is pressed and player is not on edge of map decrement y
+        }else if (this.ctlKey === 'down' && this.y != 500){
+            this.y = this.y + 50;
+        }
+        this.ctlKey = null;
+
+        //Player dies if steps in lava
+        if(this.x >= 50 && this.x <= 101 && this.y <= 202 && this.y >= 151){
+                alert("WHOA! You just stepped in lava! Not cool, bro. Not cool.");
+                this.dead();
+        }
+
+        if(this.x >= 252 && this.x <= 303 && this.y <= 202 && this.y >= 151){
+                alert("WHOA! You just stepped in lava! Not cool, bro. Not cool.");
+                this.dead();
+        }
+
+        if(this.x >= 454 && this.x <= 505 && this.y <= 202 && this.y >= 151){
+                alert("WHOA! You just stepped in lava! Not cool, bro. Not cool.");
+                this.dead();
+        }
+
+        if(this.x >= 656 && this.x <= 707 && this.y <= 202 && this.y >= 151){
+                alert("WHOA! You just stepped in lava! Not cool, bro. Not cool.");
+                this.dead();
+        }
+        //If three gems collected and on water, player wins.
+        if(this.y < 25 && gemsObtained >= 3){
+            this.win();
+        } else if (this.y < 25 && gemsObtained < 3) {
+            alert("You need at least three gems to pay for your voyage!");
+            this.reset();
+        }
+    }
+}
+
+
+//Input handler for player
+Player.prototype.handleInput = function(e){
+    this.ctlKey = e;
+}
+
+
+// Instantiate enemies objects
+var allEnemies = [];
+(function setEnemies(){
+    allEnemies.push(new Gatekeeper(250, 60));
+    allEnemies.push(new Enemy(-2, 100));
+    allEnemies.push(new Enemy(-2,150));
+    allEnemies.push(new FlyingEnemy(910, 220));
+    allEnemies.push(new Weeds(150, 630));
+    allEnemies.push(new Weeds(50, 630));
+    allEnemies.push(new Weeds(90, 630));
+}());
+
+var gatekeeper = new Gatekeeper();
+var player = new Player();
+// listens for key presses and sends the keys to
+// Player.handleInput() method.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
---
+++ js/m-engine.js
@@ -0,0 +1,106 @@
+var Engine = (function(global) {
+    var doc = global.document,
+        win = global.window,
+        canvas = doc.createElement('canvas'),
+        ctx = canvas.getContext('2d'),
+        patterns = {},
+        lastTime;
+
+    canvas.width = 909;
+    canvas.height = 707;
+    doc.body.appendChild(canvas);
+
+    function main() {
+        var now = Date.now(),
+            dt = (now - lastTime) / 1000.0;
+
+        update(dt);
+        render();
+
+        lastTime = now;
+        win.requestAnimationFrame(main);
+    };
+
+    function init() {
+
+        reset();
+        lastTime = Date.now();
+        main();
+    }
+
+    function startGame(){
+        document.getElementById('start').style.display = 'none';
+        document.getElementById('screen-overlay').style.display = 'none';
+    }
+
+    function update(dt) {
+        updateEntities(dt);
+        scoreDisplay.innerHTML = "Gems Collected: " + gemsObtained;
+        livesRemaining.innerHTML = "Lives Remaining: " + lives;
+    }
+
+    function updateEntities(dt) {
+        allEnemies.forEach(function(enemy) {
+            enemy.update(dt);
+        });
+        gemstone.update();
+        player.update();
+
+    }
+
+    function render() {
+        var rowImages = [
+                'images/water-block.png',
+                'images/stone-block.png',
+                'images/stone-block.png',
+                'images/lava-block.png',
+                'images/stone-block.png',
+                'images/grass-block.png',
+                'images/grass-block.png'
+            ],
+            numRows = 7,
+            numCols = 9,
+            row, col;
+
+        for (row = 0; row < numRows; row++) {
+            for (col = 0; col < numCols; col++) {
+                 if(col % 2 == 0 && row == 3 ){
+                    ctx.drawImage(Resources.get(rowImages[2]), col * 101, row * 83);
+                 } else{
+                ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+                }
+            }
+        }
+
+        renderEntities();
+    }
+
+    function renderEntities() {
+        allEnemies.forEach(function(enemy) {
+            enemy.render();
+        });
+        player.render();
+        gemstone.render();
+    }
+
+    Resources.load([
+        'images/stone-block.png',
+        'images/water-block.png',
+        'images/grass-block.png',
+        'images/bee.png',
+        'images/lava-block.png',
+        'images/enemy-bug.png',
+        'images/flying-bug.png',
+        'images/gem.png',
+        'images/winner.png',
+        'images/weeds.png',
+        'images/dead.png',
+        'images/grave.png',
+        'images/elfin.png',
+        'images/goodbye.png',
+        'images/char-boy.png'
+    ]);
+    Resources.onReady(init);
+
+    global.ctx = ctx;
+})(this);
