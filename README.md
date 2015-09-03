646ed32405505b6eb14f1a1241b21333779f19dc
Marc Anderson <marc_anderson_1981@yahoo.com>
2015-09-03 12:48:42 -0500

    Finished game project

---
+++ README.md
@@ -0,0 +1,44 @@
+This is a simple JavaScript based game, in which the object is to collect three gems and then proceed to the water.
+
+Use the arrow keys to move the player. The player must avoid the bugs, lava, and vines, otherwise the player loses a life.
+
+There is a three minute time limit. The timer begins its countdown when the start button is clicked.
+
+The game can be accessed and played directly by going to: http://marc1981.github.io/javascript-game
+
+Alternatively, the files can be obtained from this Github site (https://github.com/marc1981/javascript-game), either by downloading the entire zipped file-package to your computer's desktop or by forking this git.
+
+If you choose to utilize the files, the game can be accessed by opening the file called "index.html" in your internet browser (preferably Chrome or Fire Fox).
+
+If you can't figure out how to get started based on these instructions, just give up. It's hopeless.
+
+Now I have no idea what else is supposed to be in this file, so I will just explain everything. How's that? Okay this game is made using HTML, CSS, and JavaScript. It also makes use of image files. As I have stated there is one HTML file called 'index.html', and the two CSS files are called 'pop-styles.css' and 'style.css'. The HTML file sets up the canvas area and also contains the 'div' elements necessary to render the pop-up like windows seen at the start of the game and during other points in game play. The 'pop-styles.css' file contains the style elements for these pop-up windows. The 'style.css' file contains the rest of the style elements for the background and positioning of the canvas. The CSS files are located in a folder called 'css.'
+
+There are five separate JavaScript files located in a folder called 'js', and they are as follows:
+'m-app.js' - this contains the JavaScript code that governs the player's and the enemies' movements
+'m-engine.js' - this contains the JS code that sets up the canvas with its background and also sets up the top display for showing the time, number of lives left, and number of gems collected
+'m-resources.js' - the controls the most basic elements of the game loading
+'m-game-controls.js' - this code is for the countdown timer, and it also sets up the start screen for each new game
+'css-pop.js' -  this governs the action of the pop-ups
+
+The images used in this game are in PNG format. Many of the character and tile images were provided by Udacity, however the Gatekeeper, the lava, the vines, the altered bugs and altered player-character were created by me using Photoshop.
+
+This README file is ridiculously long, and it shouldn't be necessary to read it to either run or play the game. Never the less, I have been informed multiple times that this README file contains insufficient documentation, so I will continue and try to explain things as clearly as possible.
+
+Click on the start button using your mouse. Again, the timer begins its countdown when the start button is clicked, and it resets when the player either wins the game, quits the game, or loses all of their lives. Use the up arrow to move the player (the elf-like character) towards the water (up the screen). Use the left arrow button on the keyboard to move the player left. Use the right arrow button on the keyboard to move the player to the right. Use the down arrow button on the keyboard to move down. The player is confined to the playing field, which is the canvas area.
+
+For a description of where to locate the arrow buttons on your keyboard, refer to the following page: https://en.wikipedia.org/wiki/Arrow_keys
+
+For help with understanding basic instructions, refer to the following: http://courses.umass.edu/phil110-gmh/text/c01_3-99.pdf
+
+Gems are collected by running the player into them. If a gem is collected the number in the upper right hand corner of the canvas (it is called "Gems Collected") increases by one. It is necessary to collect at least 3 gems in order to win the game, but more than 3 can be obtained. The gems that the player must collect move at random, so you must be quick in order to catch them.
+
+The timer is located in the upper center of the page. It starts at 3:00 indicating three minutes. It decreases one second each second, as most countdown timers do.
+
+For help in understanding the basic units of time, refer to the following: https://en.wikipedia.org/wiki/Unit_of_time
+
+The player begins with 3 lives. A life is lost if a player collides with any of the bug enemies, walks into the lava tiles, crosses into the vine field, or answers the Gate-keeper's question incorrectly. If all 3 lives are lost, the game is over, and it resets.
+
+For assistance on counting refer to the following: http://goo.gl/m5VQnY
+
+If you are still confused on how to play this game, please seek professional help.
---
+++ js/m-app.js
@@ -0,0 +1,365 @@
+// Player class and initial x and y coordinates
+var Player = function(){
+    this.sprite = 'images/elfin.png';
+    this.x = 450;
+    this.y = 500;
+};
+
+// Draw the player object on the screen
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+//Reset player to beginning position
+Player.prototype.reset = function() {
+  this.x = 450;
+  this.y = 450;
+  this.sprite = 'images/elfin.png';
+  begin = true;
+  ctx.clearRect(0, 0, 500, 500);
+};
+
+//If player reaches water, image changes, announces winning.
+Player.prototype.win = function(){
+    this.sprite = "images/winner.png";
+    this.x = 600;
+    this.y = -50;
+    resetTimer = true;
+    lives = 3;
+    popup('winner');
+    ctx.clearRect(0, -50, 750, 750);
+    this.reset();
+};
+
+//If time runs out, announce and end game.
+Player.prototype.timesUp = function(){
+    this.sprite = "images/grave.png";
+    this.x = 405;
+    this.y = 455;
+    begin = false;
+    setTimeout(function(){ popup('timeUp'); }, 500);
+    setTimeout(function(){ popup('timeUp'); playerNewImageClear(); beginNewGame();}, 5000);
+};
+
+//If player is killed the number of lives decrements, and if no more lives, game is over. Otherwise ask player to try again.
+Player.prototype.dead = function(){
+    lives = lives - 1;
+    gemsObtained = 0;
+    if(lives <= 0){
+        this.sprite = "images/grave.png";
+        this.x = 405;
+        this.y = 455;
+        begin = false;
+        setTimeout(function(){ popup('noLivesLeft'); }, 900);
+        setTimeout(function(){ popup('noLivesLeft'); playerNewImageClear(); beginNewGame();}, 7000);
+    } else {
+        this.sprite = "images/dead.png";
+        this.x = 405;
+        this.y = 425;
+        begin = false;
+        setTimeout(function(){
+            popup('tryAgain');
+        }, 500);
+    }
+};
+
+//clear contents where dead character was and reset player.
+function playerNewImageClear(){
+    ctx.clearRect(450, 500, 100, 100);
+    player.sprite = 'images/elfin.png';
+    player.reset();
+}
+
+//show if player chooses to quit game.
+function quitGame(){
+    popup('quitPopUp');
+    player.x = 405;
+    player.y = 455;
+    player.sprite = 'images/grave.png';
+    setTimeout(function(){player.sprite = 'images/elfin.png'; popup('quitPopUp'); player.reset(); beginNewGame();}, 5000);
+}
+
+//sets initial lives and gems, and then inserts number into location on screen
+var begin = false;
+var lives = 3;
+var livesRemaining = document.getElementById('lives');
+var gemsObtained = 0;
+var scoreDisplay = document.getElementById('score');
+
+//Update player position
+Player.prototype.update = function(){
+    if(begin){//if left key is pressed and player is not on edge of map, pressed decrement x
+        if(this.ctlKey === 'left' && this.x > 0){
+            this.x = this.x - 50;
+        //if right key is pressed and player is not on edge of map increment x
+        }else if(this.ctlKey === 'right' && this.x != 800){
+            this.x = this.x + 50;
+        //if up key is pressed increment y
+        }else if(this.ctlKey === 'up'){
+            this.y = this.y - 50;
+        //if down key is pressed and player is not on edge of map decrement y
+        }else if (this.ctlKey === 'down' && this.y != 500){
+            this.y = this.y + 50;
+        }
+        this.ctlKey = null;
+
+        //Player dies if steps in lava
+        if(this.x >= 50 && this.x <= 101 && this.y <= 202 && this.y >= 151){
+                popup('lavaPopUp');
+                this.sprite = "images/dead.png";
+                this.reset();
+        }
+
+        if(this.x >= 252 && this.x <= 303 && this.y <= 202 && this.y >= 151){
+                popup('lavaPopUp');
+                this.sprite = "images/dead.png";
+                this.reset();
+        }
+
+        if(this.x >= 454 && this.x <= 505 && this.y <= 202 && this.y >= 151){
+                popup('lavaPopUp');
+                this.sprite = "images/dead.png";
+                this.reset();
+        }
+
+        if(this.x >= 656 && this.x <= 707 && this.y <= 202 && this.y >= 151){
+                popup('lavaPopUp');
+                this.sprite = "images/dead.png";
+                this.reset();
+        }
+        //If three gems collected and on water, player wins.
+        if(this.y < 25 && gemsObtained >= 3){
+            this.win();
+        } else if (this.y < 25 && gemsObtained < 3) {
+            popup('need-gems');
+            this.x = 650;
+            this.y = 60;
+            setTimeout(function(){popup('need-gems'); this.reset();}, 6000);
+
+        }
+    }
+};
+
+//Input handler for player
+Player.prototype.handleInput = function(e){
+    this.ctlKey = e;
+};
+
+// Enemies the player must avoid
+var Enemy = function(x,y) {
+
+    // The image/sprite for enemies
+    this.sprite = 'images/enemy-bug.png';
+
+    //x and y coordinates and movement speed
+    this.x = x;
+    this.y = y;
+    this.speed = Math.floor((Math.random() * 100) + 100);
+};
+
+var FlyingEnemy = function(x,y){
+    this.sprite = 'images/flying-bug.png';
+    this.x = x;
+    this.y = y;
+    this.speed = Math.floor((Math.random() * 100) + 100);
+};
+
+var Gatekeeper = function(x,y) {
+    this.sprite = 'images/bee.png';
+    //x and y coordinates and movement speed
+    this.x = x;
+    this.y = y - 10;
+    this.speed = Math.floor((Math.random() * 100) + 100);
+};
+
+// Draw the enemy objects on the screen
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+FlyingEnemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Gatekeeper.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Enemy.prototype.update = function(dt) {
+    //if the enemy crosses off screen, reset its position. Otherwise, it keeps running.
+    if(this.x <= 850){
+        this.x += this.speed * dt;
+    }else{
+        this.x = -2;
+    }
+
+    //If the player comes within 30px of an enemy's x and y coordinates, reset the game
+    if(player.x >= this.x - 30 && player.x <= this.x + 30){
+        if(player.y >= this.y - 30 && player.y <= this.y + 30){
+            popup('bugPopUp');
+            player.sprite = "images/dead.png";
+            player.reset();
+        }
+    }
+};
+
+FlyingEnemy.prototype.update = function(dt) {
+    //if the enemy crosses off screen, reset its position. Otherwise, it keeps running.
+    if(this.x >= -50){
+        this.x -= this.speed * dt;
+    }else{
+        this.x = 910;
+    }
+
+    //If the player comes within 30px of an enemy's x and y coordinates, reset the game
+    if(player.x >= this.x - 30 && player.x <= this.x + 30){
+        if(player.y >= this.y - 30 && player.y <= this.y + 30){
+            popup('flyingBugPopUp');
+            player.sprite = "images/dead.png";
+            player.reset();
+        }
+    }
+};
+
+var Weeds = function(x,y) {
+    this.sprite = 'images/weeds.png';
+    //x and y coordinates and movement speed
+    this.x = x;
+    this.y = y;
+    this.speed = 100;
+};
+
+Weeds.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+//Controls up and down movement of weeds
+var moveUp = true;
+var moveDown = false;
+Weeds.prototype.update = function(dt){
+    if(moveUp && this.y >= 550){
+        this.y -= this.speed * dt;
+    } else {
+        moveUp = false;
+        moveDown = true;
+    }
+    if(moveDown && this.y <= 650){
+        this.y += this.speed * dt;
+    } else {
+        moveDown = false;
+        moveUp = true;
+    }
+    ctx.clearRect(50, 600, 220, 220);
+//If player is caught in weeds, player dies.
+    if(player.x >= this.x - 30 && player.x <= this.x + 30){
+        if(player.y >= this.y - 50 && player.y <= this.y + 40){
+            popup('vinePopUp');
+            player.sprite = "images/dead.png";
+            player.reset();
+        }
+    }
+};
+
+//Sets default movement pattern for Gatekeeper
+var moveRight = true;
+var moveLeft = false;
+//controls the movement of Bee Gatekeeper
+Gatekeeper.prototype.update = function(dt) {
+    //Sets bounds and direction of movement
+    if(moveRight && this.x <= 600){
+            this.x += this.speed * dt;
+    } else {
+        moveRight = false;
+        moveLeft = true;
+    }
+    if(moveLeft && this.x >= 200){
+        this.x -= this.speed *dt;
+    } else {
+        moveLeft = false;
+        moveRight = true;
+    }
+
+    //If the player comes in contact with GateKeeper's x and y coordinates, prompt question
+    if(player.x >= this.x - 30 && player.x <= this.x + 30){
+        if(player.y >= this.y - 30 && player.y <= this.y + 30){
+            popup('riddle');
+            begin = false;
+            this.x = 200;
+            player.y = 60;
+            player.x = 660;
+        }
+    }
+};
+
+//Gem
+var Gemstone = function(x,y){
+    this.sprite = 'images/gem.png';
+    this.x = x;
+    this.y = y;
+
+};
+
+Gemstone.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+//If the player comes within 40px of an gems's x and y coordinates, gem dissappears and score increments by one.
+Gemstone.prototype.update = function(){
+   if(player.x >= this.x - 40 && player.x <= this.x + 40){
+        if(player.y >= this.y - 40 && player.y <= this.y + 40){
+            this.x = Math.floor((Math.random() * 700));
+            this.y = Math.floor((Math.random() * 505));
+            ctx.clearRect(32, 0, 520, 220);
+            ++gemsObtained;
+        }
+    }
+};
+
+var gemstone = new Gemstone(606, 404);
+
+//Controls random positioning of gemstone that changes after 7 seconds.
+var randomX = Math.floor((Math.random() * 700));
+var randomY = Math.floor((Math.random() * 505));
+var randomMove = function(){
+        if(randomY >= 60 && randomX >= (20 + player.x) ){
+            gemstone.x = randomX;
+            gemstone.y = randomY;
+            } else {
+                if(gemstone.x <= 600){
+                gemstone.x = player.x + 100;
+                gemstone.y = 110;
+                } else{
+                    gemstone.x = player.x - 100;
+                    gemstone.y = 110;
+                }
+            }
+        };
+
+setInterval(function(){ randomMove(); }, 7000);
+
+// Instantiate enemies objects
+var allEnemies = [];
+(function setEnemies(){
+    allEnemies.push(new Gatekeeper(250, 60));
+    allEnemies.push(new Enemy(-2, 100));
+    allEnemies.push(new Enemy(-2,150));
+    allEnemies.push(new FlyingEnemy(910, 220));
+    allEnemies.push(new Weeds(150, 630));
+    allEnemies.push(new Weeds(50, 630));
+    allEnemies.push(new Weeds(90, 630));
+}());
+
+var gatekeeper = new Gatekeeper();
+var player = new Player();
+// listens for key presses and sends the keys to
+// Player.handleInput() method.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
